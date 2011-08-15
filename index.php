a9390b57f96d4bf4a0f5214d91caee37ed5740d2
Scott Bates <scott.bates@22greystreet.com>
2011-08-15 22:58:09 +0000

    reverting grid map to 10km grid, last commit had pushed to a 1km grid in development which is not ready yet.

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $tmpl->place('header');
 								view_by: "grid"
 							});
 							gm_load_feeds({
-								json: new Array("/feeds/gridFusionSmall.json"),
+								json: new Array("/feeds/gridFusion.json"),
 							});
 							create_maps();
 						</script>					
