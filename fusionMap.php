8e4cffd2571665f9341fc0e57a9cb2673819d776
Kalin KOZHUHAROV <me.kalin@gmail.com>
2011-08-26 14:15:07 +0000

    forgot to add these...

--- fusionMap.php
+++ fusionMap.php
@@ -23,29 +23,28 @@ $tmpl->set('lang', $lang);
 
 $tmpl->place('header');
 ?>
-			<div class="content">
-				<div class="relativeWrap">
-					<div class="fullScreenWidth">
-						<div id="fusion_canvas" style="position: relative;"></div>
-						<div id="info_under" style="position:absolute; top: 0px; right: 0px; height:6em; width:200em; background-color:gray; opacity:0.85;">
-							<span id="info_size" style="position:absolute; z-index:-1; visibility: hidden">__2011-07-09T18:35:29.000+0900__</span>
-						</div>
-						<div id="info" style="position:absolute; top: 0px; right: 0px; height:6em; width:200em;" onclick="go_back();"></div>
-						<div id="search" style="position: absolute; top: 4.5em; right:0;">
-							<input type="text" size="10" id="addr" value="Go to..." onkeydown="if (event.keyCode == 13) { center_map(); return false;}" onfocus="this.select()"; onclick="this.select();" style="background: #0bb;" />
-						</div>
-						<div id="legend" style="position:absolute; top: 6em; right: 0px;">
-							<img src="images/fusionLegend.png" height="120px" width="80px" alt="legend" />
-						</div>
-					</div>
-				</div>
-				<script type="text/javascript">
-				<!--
-					document.getElementById('info_under').style.width  = document.getElementById('info_size').clientWidth + 'px'; 
-					document.getElementById('info_under').style.height = document.getElementById('info_size').clientHeight * 4.5 + 'px'; 
-					document.getElementById('info').style.width            = document.getElementById('info_size').clientWidth + 'px'; 
-					document.getElementById('info').style.height             = document.getElementById('info_size').clientHeight * 4.5 + 'px'; 
-				//--> 
-				</script>
+<div class="content">
+	<div class="relativeWrap">
+		<div id="safecast_map" style="position:relative;" class="fullScreenWidth">
+			<div id="fusion_canvas" style="position: relative;"></div>
+			<div id="info_under" style="position:absolute; top: 0px; right: 0px; height:6em; width:200em; background-color:gray; opacity:0.85;">
+				<span id="info_size" style="position:absolute; z-index:-1; visibility: hidden">__2011-07-09T18:35:29.000+0900__</span>
+			</div>
+			<div id="info" style="position:absolute; top: 0px; right: 0px; height:6em; width:200em;" onclick="go_back();"></div>
+			<div id="search" style="position:absolute; top: 6em; right:0;">
+				<input type="text" size="10" id="addr" value="Go to..." onkeydown="if (event.keyCode == 13) { center_map(); return false;}" onfocus="this.select()"; onclick="this.select();" style="background: #0bb;" />
+			</div>
+			<div id="legend" style="position:absolute; top: 7.5em; right: 0px;">
+				<img src="images/fusionLegend.png" height="120px" width="80px" alt="legend" />
+			</div>
+			<div id="logo" style="position:absolute; bottom: 10px; right: 0px;">
+				<a href="http://maps.safecast.org/" target="_blank"><img src="images/SafeCast_logo.png" height="74px" width="64px" alt="logo" /></a>
+			</div>
+			<script type="text/javascript">
+			<!--
+				document.getElementById('info').style.width = document.getElementById('info_under').style.width = document.getElementById('info_size').clientWidth + 'px'; 
+			//-->		
+			</script>
+		</div>
 
 <?php $tmpl->place('footer') ?>
